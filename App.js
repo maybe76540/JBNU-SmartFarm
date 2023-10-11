0bcb1238698b788f96e51230fa90aa84758240c4
EunSeok <maybe76540@gmail.com>
2023-10-11 18:33:24 +0900

    Initial commit

--- App.js
+++ App.js
@@ -4,8 +4,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Text style={{fontSize:20, fontWeight:'bold'}}>
+          JBNU 스마트팜 IoT 서비스</Text>
+      </View>
+
+      <View style={styles.stateBox}>
+        <View style={styles.CelstateBox}>
+          <Text style={{fontSize:20, fontWeight:'bold'}}>
+            현재 온도</Text>
+          <Text>예시 섭씨 20°C</Text>
+        </View>
+        <View style={styles.HumstateBox}>
+          <Text style={{fontSize:20, fontWeight:'bold'}}>
+            현재 습도</Text>
+          <Text>예시 습도 50%</Text>
+        </View>
+      </View>
+      
+
     </View>
   );
 }
@@ -16,5 +35,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'baseline',
+    paddingTop: 50,
+  },
+  stateBox: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  CelstateBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20,
+    /*backgroundColor: 'gray',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,*/
+  },
+  HumstateBox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 20,
   },
 });
