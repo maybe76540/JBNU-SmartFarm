304fff27e2afdf82b229a43012ca40789ad9bd8d
EunSeok OH <maybe76540@gmail.com>
2023-11-06 18:29:03 +0900

    add user Location

--- App/Components/Map/MapView.js
+++ App/Components/Map/MapView.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import useLocation from './useLocation';
 
 export default function MapVeiw() {
+
+  //현재 사용자 위치 불러오는 코드
+  const { city, region, district, mapRegion } = useLocation();
+
   return (
     <SafeAreaView>
       <MapView // 셀프클로징해도 되지만 후의 마커를 위해서
@@ -13,7 +19,10 @@ export default function MapVeiw() {
                 latitudeDelta: 0.01,
                 longitudeDelta: 0.01,
               }}
-        provider={PROVIDER_GOOGLE}/>
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        region={mapRegion}/>
     </SafeAreaView>
   )
 }
---
+++ App/Components/Map/useLocation.js
@@ -0,0 +1,58 @@
+// useLocation.js
+//현재 사용자 위치정보 접근과 handling 파일
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
+export default function useLocation() {
+  const [locationData, setLocationData] = useState({
+    city: '',
+    region: '',
+    district: '',
+    mapRegion: null,
+  });
+
+  useEffect(() => {
+    let subscription = null;
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('위치정보에 대한 접근이 거부되었습니다');
+        return;
+      }
+
+      subscription = await Location.watchPositionAsync(
+        {
+            accuracy: Location.Accuracy.High,
+        },
+        async (location) => {
+            const {coords} = location;
+            const {latitude, longitude} = coords;
+
+            const findCity = await Location.reverseGeocodeAsync(
+              {latitude, longitude}, {useGoogleMaps: false});
+
+            setLocationData({
+              city: findCity[0].city,
+              region: findCity[0].region,
+              district: findCity[0].district,
+              mapRegion: {
+                latitude: latitude,
+                longitude: longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+              },
+            });
+        },
+      );
+    })();
+
+    return () => {
+        if(subscription) {
+            subscription.remove();
+        }
+    };
+  }, []);
+
+  return locationData;
+}
