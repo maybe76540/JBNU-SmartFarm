f3334313f7f8d35413e2e2bb726d1a5fb09b2ba8
EunSeok OH <maybe76540@gmail.com>
2023-11-12 11:51:07 +0900

    Merge remote-tracking branch 'origin/app'

--- App/Components/Map/MapView.js
+++ App/Components/Map/MapView.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, SafeAreaView } from 'react-native'
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
 import useLocation from './useLocation';
+
 
 export default function MapVeiw() {
 
